3a93293294f901c5b8092e8f5a0f2477f1838029
ABDOU <abdouchaibdraa@outlook.com>
2023-12-20 00:53:42 +0100

    teacher sigin

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,13 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+// add admin eventListner for teacher radio button
+
+var radioBtn = document.getElementById('teacher-radio');
+var select = document.getElementById('grade');
+
+radioBtn.addEventListener('change' , function() {
+    select.style.display = this.checked ? 'block' : 'none' ;
+});
+

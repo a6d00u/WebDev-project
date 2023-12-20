e8c3c9283fe979b52fc7e8774afb9b9f7481b089
ABDOU <abdouchaibdraa@outlook.com>
2023-12-20 21:59:59 +0100

    fix signin bug

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,10 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+const radioBtn = document.getElementById('teacher-radio');
+const studentBtn = document.getElementById('student')
+const select = document.getElementById('grade');
+
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
@@ -12,10 +16,10 @@ loginBtn.addEventListener('click', () => {
 
 // add admin eventListner for teacher radio button
 
-var radioBtn = document.getElementById('teacher-radio');
-var select = document.getElementById('grade');
-
 radioBtn.addEventListener('change' , function() {
     select.style.display = this.checked ? 'block' : 'none' ;
+});
+studentBtn.addEventListener('change' , function() {
+    select.style.display = this.checked ? 'none' : 'block' ;
 });
 
